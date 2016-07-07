534c9ca0a04552bf2caba03950de2a5ebeded7df
hiromipaw <silvia@nopressure.co.uk>
2016-07-07 13:53:39 +0200

    Add loggin in worker

--- app/workers/geo_cache_worker.rb
+++ app/workers/geo_cache_worker.rb
@@ -5,6 +5,7 @@ class GeoCacheWorker
   recurrence { hourly.minute_of_hour(0, 10, 20, 30, 40, 50) }
 
   def perform(query_params = {})
+    logger.warn "[Sidekiq] Performing geo cache"
     Asset.get_mongo_assets(query_params, "mongo_geo_assets")
   end
 end
