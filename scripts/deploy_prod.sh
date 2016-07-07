2a4c52de202875d924785b4d1140cd6b6871c122
hiromipaw <silvia@nopressure.co.uk>
2016-07-07 12:09:26 +0200

    Add skylight config for worker

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+git fetch --all;
+git reset --hard origin/master;
 docker-compose build && docker-compose up -d && ./scripts/clean.sh
--- scripts/sidekiq.sh
+++ scripts/sidekiq.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+bundle exec skylight setup $SKYLIGHT;
 bundle exec sidekiq -e $RAILS_ENV -c 25 -C config/sidekiq.yml;
