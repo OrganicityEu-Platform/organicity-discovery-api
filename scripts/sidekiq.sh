5096b5ac36b759f8e5feb6b815317e780649cd08
hiromipaw <silvia@nopressure.co.uk>
2016-07-07 13:36:08 +0200

    Modify Sidekiq config

--- scripts/sidekiq.sh
+++ scripts/sidekiq.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 bundle exec skylight setup $SKYLIGHT;
-bundle exec sidekiq -e $RAILS_ENV -c 25 -C config/sidekiq.yml;
+bundle exec sidekiq -e $RAILS_ENV -C config/sidekiq.yml -q critical -q default -q low;
