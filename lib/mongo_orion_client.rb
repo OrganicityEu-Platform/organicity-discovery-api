b220e9edd810ccb1e20aa1f6f31adb181ccf42bd
hiromipaw <silvia@nopressure.co.uk>
2016-07-13 16:15:59 +0200

    Add flex queries in mongo and refactor

--- lib/mongo_orion_client.rb
+++ lib/mongo_orion_client.rb
@@ -13,31 +13,27 @@ module MongoOrionClient
     mongo_client.db('orion')
   end
 
-  def create_query
-    qbuilder = {}
-    if params[:site] and params[:service] and params[:provider] and params[:group]
-      qbuilder = { '_id.id' => /.*#{params[:site]}.#{params[:service]}.#{params[:provider]}.#{params[:group]}.*/ }
-    elsif params[:site] and params[:service] and params[:provider]
-      qbuilder = { '_id.id' => /.*#{params[:site]}.#{params[:service]}.#{params[:provider]}.*/ }
-    elsif params[:site] and params[:service]
-      qbuilder = { '_id.id' => /.*#{params[:site]}.#{params[:service]}.*/ }
-    elsif params[:site]
-      qbuilder = { '_id.id' => /.*#{params[:site]}.*/ }
-    elsif params[:service]
-      qbuilder = { '_id.id' => /.*#{params[:service]}.*/ }
-    elsif params[:provider]
-      qbuilder = { '_id.id' => /.*#{params[:provider]}.*/ }
-    elsif params[:group]
-      qbuilder = { '_id.id' => /.*#{params[:group]}.*/ }
-    end
+  def create_options(params)
+    mbuilder = {}
+    o = offset(params)
+    l = limit(params)
+    s = sort_query(params)
+
+    mbuilder.merge!(:skip => o) if o
+
+    mbuilder.merge!(:limit => l) if l
+
+    mbuilder.merge!(:sort => s) if s
+
+    return mbuilder
+  end
+
+  def create_query(params)
+    qbuilder = { '_id.id' => /.*#{params[:site] ? params[:site] : '*'}.#{params[:service] ? params[:service] : '*'}.#{params[:provider] ? params[:provider] : '*'}.#{params[:group] ? params[:group] : '*'}.*/ }
 
     if params[:long] and params[:lat] and params[:radius]
       qbuilder.merge!("location.coords.coordinates" => {
-        '$geoWithin': { '$center': [ [  params[:long].to_f, params[:lat].to_f ], params[:radius].to_i ] }
-      })
-    elsif params[:long] and params[:lat]
-      qbuilder.merge!("location.coords.coordinates" => {
-        '$geoWithin': { '$center': [ [  params[:long].to_f, params[:lat].to_f ], 1 ] }
+        '$geoWithin': { '$center': [ [  params[:long].to_f, params[:lat].to_f ], "#{params[:radius] ? params[:radius] : 1}".to_i ] }
       })
     end
 
@@ -47,69 +43,7 @@ module MongoOrionClient
 
     # Missing Search metadata and attributes:
     # 'attr.#{attr_name}' => /.*#{value}.*/
-  end
-
-  def mongo_geo_search_assets(params)
-    orion = setup_client
-    orion[:entities].find(
-      {
-        'location.coords.coordinates' => {
-          '$geoWithin': { '$center': [
-            [  params[:long].to_f, params[:lat].to_f ],
-            params[:radius].to_i
-          ] }
-        }
-      },
-      {
-        :skip => offset(params),
-        :limit => limit(params),
-        :sort => sort_query(params)
-      }
-    ).to_a
-  end
-
-  def mongo_site_assets(params)
-    orion = setup_client
-    orion[:entities].find(
-      {
-        '_id.id' => /.*#{params[:site]}.*/,
-      },
-      {
-        :skip => offset(params),
-        :limit => limit(params),
-        :sort => sort_query(params)
-      }
-    ).to_a
-  end
-
-  def mongo_provider_assets(params)
-    orion = setup_client
-    orion[:entities].find(
-      {
-        '_id.id' => /.*#{params[:provider]}.*/,
-      },
-      {
-        :skip => offset(params),
-        :limit => limit(params),
-        :sort => sort_query(params)
-      }
-    ).to_a
-
-  end
-
-  def mongo_service_assets(params)
-    orion = setup_client
-    orion[:entities].find(
-      {
-        '_id.id' => /.*#{params[:service]}.*/,
-      },
-      {
-        :skip => offset(params),
-        :limit => limit(params),
-        :sort => sort_query(params)
-      }
-    ).to_a
-
+    return qbuilder
   end
 
   def mongo_geo_assets(params)
@@ -119,16 +53,15 @@ module MongoOrionClient
   end
 
   def mongo_assets(params)
-    log params
+    logger.warn params
     orion = setup_client
-    q = create_query
+    q = create_query(params)
+    m = create_options(params)
+    logger.warn q
+    logger.warn m
     orion[:entities].find(
       q,
-      {
-        :skip => offset(params),
-        :limit => limit(params),
-        :sort => sort_query(params)
-      }
+      m
     ).to_a
   end
 
@@ -146,17 +79,14 @@ module MongoOrionClient
   end
 
   def mongo_asset(params)
-    logger.warn "HELLO!"
-    a = {
-          '_id.id' => /#{params[:id]}/,
-        }
-    b = {
-          :limit => 1
-        }
-
     orion = setup_client
     orion[:entities].find(
-      a, b
+      {
+        '_id.id' => /#{params[:id]}/,
+      },
+      {
+        :limit => 1
+      }
     ).to_a
 
   end
@@ -183,10 +113,10 @@ module MongoOrionClient
   end
 
   def limit(params)
-    return params[:per].to_i
+    return params[:per].to_i unless params[:per].blank?
   end
 
   def offset(params)
-    return (params[:page].to_i - 1) * params[:per].to_i
+    return (params[:page].to_i - 1) * params[:per].to_i unless params[:page].blank?
   end
 end
