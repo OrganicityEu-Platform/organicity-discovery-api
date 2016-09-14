47d09e828994d3c26666fa80bea32e04f8e0e980
hiromipaw <silvia@nopressure.co.uk>
2016-09-14 10:06:03 +0200

    Fixing nearby

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,8 +1,8 @@
 class City < ApplicationRecord
   has_many :links, as: :linkable
 
-  scope :close_to, -> (latitude, longitude, distance_in_meters = 2000) {
+  scope :nearby, -> (latitude, longitude, distance_in_meters = 2000) {
     lonlat = "POINT(#{longitude} #{latitude})"
-    order("ST_Distance(lonlat, ST_GeomFromText('#{lonlat.as_text}',4326))").limit(10)
+    order("ST_Distance(lonlat, ST_GeomFromText('#{lonlat}',4326))").limit(10)
   }
 end
