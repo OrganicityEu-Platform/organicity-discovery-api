c373ef13c80c2b2bfd48013532ec96ccf381a00f
hiromipaw <silvia@nopressure.co.uk>
2016-09-14 09:48:32 +0200

    Add close_to scope in cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,15 @@
 class City < ApplicationRecord
   has_many :links, as: :linkable
+
+  scope :close_to, -> (latitude, longitude, distance_in_meters = 2000) {
+    where(%{
+      ST_DWithin(
+        ST_GeographyFromText(
+          'SRID=4326;POINT(' || cities.longitude || ' ' || cities.latitude || ')'
+        ),
+        ST_GeographyFromText('SRID=4326;POINT(%f %f)'),
+        %d
+      )
+    } % [longitude, latitude, distance_in_meters])
+  }
 end
