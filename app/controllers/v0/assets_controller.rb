35dbf3d8dd43b5b1f2752093415bb382d9103a55
hiromipaw <silvia@nopressure.co.uk>
2016-09-26 16:17:57 +0200

    Fix metadata search

--- app/controllers/v0/assets_controller.rb
+++ app/controllers/v0/assets_controller.rb
@@ -36,7 +36,7 @@ class V0::AssetsController < ApplicationController
 
   def metadata_search
     query_params = map_query_parameters(params)
-    @assets = JSON.parse(Asset.get_mongo_assets(query_params, "mongo_metadata_search_assets", request, token_session(params[:token])))
+    @assets = Asset.get_mongo_assets(query_params, "mongo_metadata_search_assets", request, token_session(params[:token]))
     logger.warn @assets
     render json: @assets, each_serializer: AssetSerializer, root: false
   end
