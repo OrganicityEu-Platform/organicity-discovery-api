79b4119d8c883d4b3e895206fe0d107150bcf7d1
Viktor Smari <viktorsmari@gmail.com>
2017-07-18 14:05:38 +0200

    Add logging

--- app/controllers/concerns/jwt_operations.rb
+++ app/controllers/concerns/jwt_operations.rb
@@ -6,6 +6,10 @@ module JwtOperations
   # In the future check https://github.com/nsarno/knock
 
   def decode_token(token)
+    # TODO: JWT::DecodeError (Not enough or too many segments)
+    # app/controllers/concerns/jwt_operations.rb:17:in `token_session'
+    # app/controllers/v0/assets_controller.rb:71:in `geo_count'
+    logger.warn token
     return JWT.decode token, nil, false
   end
 
