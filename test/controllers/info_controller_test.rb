a33357f460e2a79bbe34c9266a68d5ec787cdb91
Viktor Smari <viktorsmari@gmail.com>
2017-08-31 18:34:35 +0200

    Add test for / checking response.body

--- test/controllers/info_controller_test.rb
+++ test/controllers/info_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class InfoControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-#    get info_index_url
-#    assert_response :success
+    get '/'
+    assert_response :success
+  end
+
+  test "info contains" do
+    get '/'
+    assert_includes response.body,"Nothing her"
   end
 
 end
