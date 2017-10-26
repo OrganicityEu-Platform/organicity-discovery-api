8bcf6f2e8f7dcd3845f54933e14c3b930e608253
Viktor Smari <viktorsmari@gmail.com>
2017-10-26 13:25:01 +0200

    Skip an empty test

--- test/workers/mongo_worker_test.rb
+++ test/workers/mongo_worker_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test_helper'
 class MongoWorkerTest < Minitest::Test
   def test_example
-    skip "add some examples to (or delete) #{__FILE__}"
+    #skip "add some examples to (or delete) #{__FILE__}"
   end
 end
