7c13679a857d91519cd761a63d015d76b5988ae6
Luis Diez <ldiez@tlmat.unican.es>
2017-12-15 17:47:56 +0100

    New OC sites
    I've also added an extra one for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,14 +50,61 @@ leuven = City.find_or_create_by!(
   id: 5,
   name: 'Leuven',
   description: 'Leuven',
-  latitude: 38.25,
-  longitude: 21.733333,
-  lonlat: 'POINT(21.733333 38.25)',
-  country_code: 'GR',
-  country: 'Greece',
-  region: 'Western Greece'
+  latitude: 50.879472,
+  longitude: 4.701072,
+  lonlat: 'POINT(4.701072 50.879472)',
+  country_code: 'BE',
+  country: 'Belgium',
+  region: 'Vlaams Brabant'
 )
 
+edinburgh = City.find_or_create_by!(
+  id: 6,
+  name: 'Edinburgh',
+  description: 'Edinburgh',
+  latitude: 55.9327,
+  longitude:  -2.9814,
+  lonlat: 'POINT(-2.9814 55.9327)',
+  country_code: 'UK',
+  country: 'United Kingdom',
+  region: ''
+)
+
+lisbon = City.find_or_create_by!(
+  id: 7,
+  name: 'Lisbon',
+  description: 'Lisbon',
+  latitude: 38.714006,
+  longitude: -9.133467,
+  lonlat: 'POINT(-9.133467 38.714006)',
+  country_code: 'PT',
+  country: 'Portugal',
+  region: 'Regiao de Lisboa/ Lisboa'
+)
+
+novisad = City.find_or_create_by!(
+  id: 8,
+  name: 'Novisad',
+  description: 'Novisad',
+  latitude: 45.267136,
+  longitude: 19.833549,
+  lonlat: 'POINT(19.833549 45.267136)',
+  country_code: 'RS',
+  country: 'Serbia',
+  region: 'Serbia'
+)
+
+ldieztest = City.find_or_create_by!(
+  id: 8,
+  name: 'LdiezTest',
+  description: 'LdiezTest',
+  latitude: 43.45487,
+  longitude: -3.81289,
+  lonlat: 'POINT(-3.81289 43.45487)',
+  country_code: 'ES',
+  country: 'Spain',
+  region: 'Cantabria'
+)
 
 santander.links.find_or_create_by!(
   url: 'https://en.wikipedia.org/wiki/Santander,_Spain',
@@ -85,6 +132,26 @@ patras.links.find_or_create_by!(
 )
 
 leuven.links.find_or_create_by!(
-  url: 'https://en.wikipedia.org/wiki/Patras',
+  url: 'https://en.wikipedia.org/wiki/Leuven',
+  relationship: 'wiki'
+)
+
+edinburgh.links.find_or_create_by!(
+  url: 'https://en.wikipedia.org/wiki/Leuven',
+  relationship: 'wiki'
+)
+
+lisbon.links.find_or_create_by!(
+  url: 'https://en.wikipedia.org/wiki/Leuven',
+  relationship: 'wiki'
+)
+
+novisad.links.find_or_create_by!(
+  url: 'https://en.wikipedia.org/wiki/Leuven',
+  relationship: 'wiki'
+)
+
+ldieztest.links.find_or_create_by!(
+  url: 'https://www.unican.es',
   relationship: 'wiki'
 )
