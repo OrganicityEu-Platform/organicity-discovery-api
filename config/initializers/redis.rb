d2b9f4eb63c2e22fece13c8ca1145db653b65b0a
Viktor Smari <viktorsmari@gmail.com>
2018-03-07 13:04:05 +0100

    Allow some tests to run without starting docker

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
+#By default, the client will try to read the REDIS_URL environment variable and use that as URL to connect to.
 uri = URI.parse(Rails.application.secrets.redis_url || ENV['REDIS_URL'])
 $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #byebug
+  santander = City.first
+  london = City.second
+
+  test "should match city name" do
+    assert_equal 'Santander', santander.name
+    assert_equal 'London', london.name
+  end
+
+  test "should have coordinates" do
+    assert_same 43.45487, santander.latitude
+    assert_same -3.81289, santander.longitude
+  end
 end
